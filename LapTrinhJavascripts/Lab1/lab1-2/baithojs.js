8ea551c93477ba31ee2ebb0bdbaa9c6b1dfd2ae4
NguyenDinhTu <dinhtu20091998@gmail.com>
2025-04-16 22:30:25 +0700

    ngày 16 tháng 4

--- LapTrinhJavascripts/Lab1/lab1-2/baithojs.js
+++
@@ -1,5 +0,0 @@
-
-    document.writeln("<br>Con cò xấu xí 3");
-    document.writeln("<br>Nó đậu cành me");
-    document.writeln("<br>Đi lung tung vậy");
-    document.writeln("<br>Đi vào hố sâu");
